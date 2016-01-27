81b8da00ab41e6ab23ec1302f33beb78f090c44c
王海洋 <wanghaiyang@luojilab.com>
2016-01-27 18:55:07 +0800

    添加电商平台报表功能

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -9,10 +9,11 @@ class WelcomeController extends Base{
     
     use Trait_Layout;
     
-    public $wel_model;
+    public $sdata_model;
     
     public function init(){
         $this->initAdmin();
+        $this->sdata_model = new SdataModel();
     }
     
     /**
@@ -21,6 +22,14 @@ class WelcomeController extends Base{
     public function indexAction(){
         $this->checkLogin();
 //        $this->checkRole();
+        if($_POST){
+            $time1 = $this->getRequest()->getPost('time1');
+            $params['start_created'] = $time1;
+            $params['end_created'] = date('Y-m-d', strtotime($time1.'+1 day'));
+            $result = $this->sdata_model->getList($params);
+            exit;
+        }
+        $this->assign('time', date('Y-m-d', time()));
         $this->layout('welcome/index.phtml');
     }
 }
