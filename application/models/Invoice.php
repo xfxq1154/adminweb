7eeba2470ceb83f470968343166f39c315bf7f91
王海洋 <wanghaiyang@luojilab.com>
2016-05-12 15:41:39 +0800

    repair bug

--- application/models/Invoice.php
+++ application/models/Invoice.php
@@ -335,7 +335,7 @@ class InvoiceModel{
      */
     public function dirtyData(){
         try{
-            $sql = " SELECT * FROM `dirty_data` WHERE `state` = 5 ";
+            $sql = " SELECT * FROM `dirty_data` WHERE `state` != 4 ";
             $stmt = $this->dbMaster->prepare($sql);
             $stmt->execute();
             return $stmt->fetchAll(PDO::FETCH_ASSOC);
