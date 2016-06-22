56b20775a66988a32fbce65253abbad13111e4f2
monky <yanbo@luojilab.com>
2016-06-22 12:57:21 +0800

    修复店铺操作记录表翻页问题

--- application/controllers/Oplogs.php
+++ application/controllers/Oplogs.php
@@ -40,16 +40,16 @@ class OplogsController extends Base {
         $nickname = $this->getRequest()->get('nickname');
         $start_time = $this->getRequest()->get('start_time');
         $end_time = $this->getRequest()->get('end_time');
-        $p = (int) $this->getRequest()->getParam('p', 1);
+        $p = (int) $this->getRequest()->get('page_no', 1);
         $page_size = 20;
 
-        $oplogs_list = $this->oplogs_model->getlist($showcase_id, $sourceid, $title, $uri, $nickname, $start_time, $end_time, $page_no, $page_size);
+        $oplogs_list = $this->oplogs_model->getlist($showcase_id, $sourceid, $title, $uri, $nickname, $start_time, $end_time, $p, $page_size);
 
         $this->assign('showcase_id', $showcase_id);
         $this->assign('params', $this->getRequest()->getQuery());
         $this->assign('showcase_list', $showcase_list['showcases']);
         $this->assign("list", $oplogs_list['oplogs']);
-        $this->renderPagger($p, $oplogs_list['total_nums'], "/oplogs/showlist/showcase_id/$showcase_id/page_no/{p}", $page_size);
+        $this->renderPagger($p, $oplogs_list['total_nums'], "/oplogs/showlist/showcase_id/$showcase_id/page_no/{p}?sourceid=$sourceid&title=$title&uri=$uri&nickname=$nickname&start_time=$start_time&end_time=$end_time", $page_size);
         $this->layout("platform/oplogs.phtml");
     }
 }
