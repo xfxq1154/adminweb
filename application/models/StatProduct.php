79eeaada12a9a7ac2265bfc1de90e998117ad6e0
monky <yanbo@luojilab.com>
2016-08-30 13:56:57 +0800

    增加pc端预览链接

--- application/models/StatProduct.php
+++ application/models/StatProduct.php
@@ -9,21 +9,23 @@ class StatProductModel {
     const PRODUCT_LIST = 'api/product/getlist_by_pageid';
 
     public function productList($params) {
-        return Sdata::request(self::PRODUCT_LIST, $params);
+        $result = Sdata::request(self::PRODUCT_LIST, $params);
+        return $this->format_datas_batch($result);
     }
 
     /**
      * 格式化
      */
-    public function format_pages_struct($data) {
+    public function format_data_struct($data) {
         if (empty($data)) {
             return array();
         }
         $format_data = array(
             'showcase_id'  =>   $data['showcase_id'],
-            'page_id'      =>   $data['page_id'],
-            'page_title'   =>   $data['page_title'],
-            'page_url'     =>   STORE_H5_HOST . 'product/detail?alias='. $data['page_id'],
+            'page_title'   =>   $data['title'],
+            'page_url'     =>   STORE_H5_HOST . 'product/detail?alias='. $data['palias'],
+            'trans_num'    =>   $data['trans_num'],
+            'trans_amount' =>   $data['trans_amount'],
             'total_pv'     =>   intval($data['total_pv']),
             'total_uv'     =>   intval($data['total_uv']),
             'share_pv'     =>   intval($data['share_pv']),
@@ -33,12 +35,12 @@ class StatProductModel {
         return $format_data;
     }
 
-    public function format_pages_batch($datas) {
+    public function format_datas_batch($datas) {
         if (empty($datas)) {
             return array();
         }
         foreach ($datas['list'] as &$data){
-            $data = $this->format_pages_struct($data);
+            $data = $this->format_data_struct($data);
         }
 
         return $datas;
