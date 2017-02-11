f2900b97d582a9bfc9238f167d4155461769da17
xfliu <273063623@qq.com>
2017-02-11 10:31:20 +0800

    first add

--- application/modules/Kf/controllers/Auth.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * @name: Auth.php
- * @time: 2016-08-25 上午11:11
- * @author: liuxuefeng
- * @desc:
- */
--- conf/storecp.config.php
+++ conf/storecp.config.php
@@ -1,6 +1,5 @@
 <?php
 define('ENVIRONMENT','develop');
-define('STORE_H5_HOST','http://h5.dev.sao.cn/');
 
 //asset
 define('ASSET_URL', 'http://storecp.lxf.dev.com/');
@@ -10,43 +9,3 @@ define('ASSET_JS_URL', ASSET_URL . '/js');
 define('ASSET_JS_PATH','/data/home/lxf/storecp/static/js');
 define('ASSET_CSS_URL', ASSET_URL . '/css');
 define('ASSET_CSS_PATH','/data/home/lxf/storecp/static/css');
-
-// img server
-define('API_SERVER_IMGURL','http://img.internal.com');
-
-define('SAPI_HOST','http://sapi.lxf.dev.com/');
-define('SAPI_SOURCE_ID','STCP0001');
-
-define('SDATA_HOST','http://sdata.lxf.dev.com/');
-define('SDATA_SOURCE_ID','WEB0001');
-
-//passport
-define('PASSPORT_HOST', 'http://passport.dev.didatrip.com/v2/');
-define('PASSPORT_SYSCODE', '3');
-
-define('PAYMENT_HOST','http://pay2.dev.didatrip.com/');
-
-//短信接口
-define('NOTIFICATION_API','http://notification.dev.didatrip.com');
-
-//图片
-define('IMG_SOURCE_ID','ADMIN0001');
-define('IMG_API_HOST','http://img.lxf.dev.com/');
-
-//电子发票
-define('DZFP_HOST', 'http://111.202.226.70:8002/zxkp_base64/services/WKWebService?wsdl');
-define('TAXPAYER_NUMBER', '110104201110174');
-
-//有赞
-define('KDT_APP_ID', 'f222469b08dcf5b3d7');
-define('KDT_APP_SECERT', '9ed217880ab4b394243cef42ec9b7c92');
-
-//商品兑换码
-define('CDKEY_HOST','http://cdkey.lxf.dev.com/');
-define('CDKEY_SOURCE_ID','IGET0001');
-
-//客服host
-define('KFAPI_HOST','http://kfapi.dev.didatrip.com/');
-
-//默认店铺
-define('DEFAULT_SHOWCASE_ID','10008');
